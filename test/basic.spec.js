08dabba1654a40d9a3c92a1b1783b113c219263e
Javier Alejandro Oyarzo <javi_14_228@hotmail.com>
2022-06-06 21:22:39 -0300

    Add Test Structure

---
+++ test/basic.spec.js
@@ -0,0 +1,30 @@
+const assert = require('chai').assert;
+const fetch = require('node-fetch');
+const { Users } = require('../database/sync');
+
+
+
+
+// PRUEBAS CHARACTERS 
+
+// Create
+
+// Update
+
+// PRUEBAS LOGIN
+
+// Login
+
+// Register
+
+// PRUEBAS MOVIES
+
+// Create
+
+// Update
+
+// PRUEBAS GENRE
+
+// Create
+
+// Update
